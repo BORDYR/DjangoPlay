10d18cae2d0ccbfcb4787dd66a001d79ebfb1f29
MihailKirillov <mishkir@mail.ru>
2020-06-16 19:50:14 +0300

    changes

--- mkproject/mkproject/settings.py
+++ mkproject/mkproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1', '172.17.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'my.apps.MyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
